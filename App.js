43c7c708439c3d1df39fdae45306150f23417e74
Matheus Mota Tonini <138072866+motaxyz@users.noreply.github.com>
2024-08-07 19:20:47 -0300

    commit

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
+import ImageViewer from './components/ImageViewer';
+
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 
@@ -8,7 +10,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        {/* <Image source={PlaceholderImage} style={styles.image} /> */}
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <StatusBar style="auto" />
     </View>
